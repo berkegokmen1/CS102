5d8f3a8cad86cacdb84edc39333aafea08704d8c
Berke Gökmen <berkegokmen1@Berkes-MacBook-Pro.local>
2021-10-08 19:38:28 +0300

    Lab01 PI calculator

---
+++ Lab01/src/PiCalculator.java
@@ -0,0 +1,36 @@
+import helpers.*;
+import java.util.Scanner;
+
+public class PiCalculator {
+
+  public static void main(String[] args) throws Exception {
+    final Scanner sc = new Scanner(System.in);
+
+    final Rectangle rectangle = new Rectangle(new Point(0, 0), 10, 10);
+    final Circle circle = new Circle(new Point(5, 5), 5);
+
+    Point p;
+    int numOfTrials;
+    int tries = 0;
+    int hits = 0;
+
+    System.out.print("Number of trials: ");
+    numOfTrials = sc.nextInt();
+
+    do {
+      p = rectangle.getRandomPoint();
+
+      if (circle.contains(p)) {
+        hits += 1;
+      }
+
+      tries += 1;
+    } while (tries <= numOfTrials);
+
+    final double piEstimation = 4.0 * hits / tries;
+
+    System.out.println("Estimated value for π: " + piEstimation);
+
+    sc.close();
+  }
+}
